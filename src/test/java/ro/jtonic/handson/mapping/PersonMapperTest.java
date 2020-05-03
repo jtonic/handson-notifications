fbf86c1f9cff44d8b6e28eae5169cc2eb2e84c17
jtonic <antonel.pazargic@gmail.com>
2020-05-03 12:28:30 +0300

    [IGNORE] Address the test -> github workflow happy again.

--- src/test/java/ro/jtonic/handson/mapping/PersonMapperTest.java
+++ src/test/java/ro/jtonic/handson/mapping/PersonMapperTest.java
@@ -2,7 +2,6 @@ package ro.jtonic.handson.mapping;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import static ro.jtonic.handson.mapping.Person.ID_DEFAULT;
 import static ro.jtonic.handson.mapping.Person.STATUS_DEFAULT;
 
@@ -32,10 +31,5 @@ class PersonMapperTest {
     assertThat(person.getName()).isEqualTo(personDto.getName());
     assertThat(person.getId()).isEqualTo(ID_DEFAULT);
     assertThat(person.getStatus()).isEqualTo(STATUS_DEFAULT);
-  }
-
-  @Test
-  void testGithubActionFailure() {
-    fail("Force fail the github workflow");
   }
 }
